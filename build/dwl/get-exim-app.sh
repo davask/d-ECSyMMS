f274e56f2ba7357b6e040fea642f1592f17bb87e
davask <contact@davaskweblimited.com>
2017-06-26 13:28:56 +0200

    Automatic update 0.1-s2.8-p7.0-a2.4-d8.8

--- build/dwl/get-exim-app.sh
+++ build/dwl/get-exim-app.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 if [ `find /home/${DWLC_USER_NAME}/files -mindepth 1 -maxdepth 1 | wc -l` == "0" ]; then
-    git clone https://github.com/davask/exim-cms.git --branch lcdd --single-branch /home/${DWLC_USER_NAME}/files;
+    echo "################################################################################################################################";
+    echo "# [RUN FROM BASH] git clone https://github.com/davask/exim-cms.git --branch lcdd --single-branch /home/${DWLC_USER_NAME}/files #";
+    echo "################################################################################################################################";
+else
+    echo "eXim app is not installed because there's file(s) in /home/${DWLC_USER_NAME}/files";
 fi
